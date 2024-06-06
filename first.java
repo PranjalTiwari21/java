03a8fe35af4a76280d6cd69c1a6717d30139885c
PranjalTiwari21 <pranjaltiwari628@gmail.com>
2024-06-06 16:21:34 +0530

    Add files via upload

---
+++ first.java
@@ -0,0 +1,26 @@
+package Tiwari;
+
+public class first {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=18;
+		int b=12;
+		//Addition of two integer numbers.
+		System.out.println("A+b ="+ (a+b));
+		
+		//Substractionof two interger numbers.
+		System.out.println("A-b ="+ (a-b));
+		
+		//Multiplications of two intergers nimbers
+		System.out.println("A*b ="+ (a*b));
+		
+		//divison of two integer numbers
+		System.out.println("A/b ="+ (a/b));
+		
+		
+
+	}
+	
+
+}
